d04867dd051476a8761f4fc666bbdcf601520120
jjuchoa <jbfdujunior@gmail.com>
2024-07-04 16:40:15 -0300

    Aula 4 video 3 Instalando o Styled Components

---
+++ src/componentes/Header/index.js
@@ -0,0 +1,22 @@
+import Logo from '../../componentes/Logo';
+import OpcoesHeader from '../../componentes/OpcoesHeader';
+import IconesHeader from '../../componentes/IconesHeader';
+import styled from 'styled-components'
+
+const HeaderContainer = styled.header`
+    background-color: #ffffff;
+    display: flex;
+    justify-content: center;
+`
+
+function Header () {
+    return (
+        <HeaderContainer>
+            <Logo/>
+            <OpcoesHeader/>       
+            <IconesHeader/>
+        </HeaderContainer>
+    )
+}
+
+export default Header
